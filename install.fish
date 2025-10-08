1703c73801178504d5b2a95602ed5d4864050b78
ChoccyHolly <choccy@realboot.net>
2025-10-08 00:49:45 -0500

    added debug messages

--- install.fish
+++ install.fish
@@ -1,18 +1,27 @@
-# Init Neovim Submodule
-git -C $HOME/.dotfiles submodule update --init --recursive
+# Download Latest Neovim Configs
+echo "Downloading Latest Neovim Configs..."
+if ls ~/.dotfiles/.config/nvim
+    git -C $HOME/.dotfiles submodule update --recursive --remote
+else
+    git -C $HOME/.dotfiles submodule update --init --recursive
+end
 
 # Remove Old config.fish
+echo "Removing Old config.fish..."
 rm ~/.config/fish/config.fish
 
 # Download Fish Theme
+echo "Downloading Fish Theme..."
 mkdir ~/.config/fish/themes
 curl https://raw.githubusercontent.com/folke/tokyonight.nvim/refs/heads/main/extras/fish_themes/tokyonight_night.theme >> ~/.config/fish/themes/tokyonight_night.theme
 
 # Download Fisher
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+echo "Downloading Fisher..."
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher >> /dev/null
 
 # Download and Configure Tide
-fisher install IlanCosman/tide@v6
+echo "Downloading and Configuring Tide..."
+fisher install IlanCosman/tide@v6 >> /dev/null
 tide configure --auto --style=Classic --prompt_colors='16 colors' --show_time=No --classic_prompt_separators=Angled --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Compact --icons='Many icons' --transient=No
 set tide_left_prompt_items os context pwd git newline character
 set tide_right_prompt_items
@@ -21,8 +30,9 @@ set tide_os_color $os_branding_color
 set tide_os_icon "$tide_os_icon "
 
 # Download Zellij on Ubuntu
-if lsb_release -a | grep "Ubuntu"
+if (lsb_release -a | grep "Ubuntu" >> /dev/null)
     if type -q zellij; else
+        echo "Downloading Zellij..."
         curl -LO "https://github.com/zellij-org/zellij/releases/latest/download/zellij-aarch64-unknown-linux-musl.tar.gz"
         tar -xf zellij-aarch64-unknown-linux-musl.tar.gz
         rm zellij-aarch64-unknown-linux-musl.tar.gz
@@ -31,6 +41,7 @@ if lsb_release -a | grep "Ubuntu"
 end
 
 # Stow Dotfiles
+echo "Stowing Dotfiles..."
 stow -d ~/.dotfiles .
 rm ~/install.fish
 
